2558831ac55d209a36f37a0da0f9e480a4c4e2d2
Mikhailnt <mikhailnt@bk.ru>
2020-10-21 22:50:08 +0300

    Перенес шаги ввода логин/пароль в фикстуру @Before

--- addressbook-web-tests/src/test/java/Package_addressbook/Test1.java
+++ addressbook-web-tests/src/test/java/Package_addressbook/Test1.java
@@ -30,13 +30,6 @@ public class Test1 {
   public void setUp() {
     WebDriverManager.firefoxdriver().setup();
     driver = new FirefoxDriver();
-     }
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void test1() {
     driver.get("http://localhost/addressbook/");
     driver.manage().window().setSize(new Dimension(1127, 701));
     driver.findElement(By.name("user")).sendKeys("admin");
@@ -44,6 +37,10 @@ public class Test1 {
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+  }
+
+  @Test
+  public void testGroupCreation() {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
@@ -54,5 +51,10 @@ public class Test1 {
     driver.findElement(By.name("group_footer")).sendKeys("tt3");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
   }
 }
